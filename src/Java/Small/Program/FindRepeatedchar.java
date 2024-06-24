3493438cc37f1543e4677a0e762ee6ca9d63146d
Shalini Mane <shalinidmane@gmail.com>
2024-06-24 09:59:08 +0530

    Third commit

--- src/Java/Small/Program/FindRepeatedchar.java
+++ src/Java/Small/Program/FindRepeatedchar.java
@@ -5,16 +5,17 @@ public class FindRepeatedchar {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		 String str = new String("Shallini");
+		 String str = new String("Shallllini");
          char[] chars = str.toCharArray();
          System.out.println("Duplicate characters are:");
          for (int i=0; i<str.length();i++) {
+        	
                      for(int j=i+1; j<str.length();j++) 
                      {
                         if (chars[i] == chars[j]) {
-                        	
+                        	 
                         System.out.println(chars[j]);
-               
+                       
                         break;
                       }
                                  
