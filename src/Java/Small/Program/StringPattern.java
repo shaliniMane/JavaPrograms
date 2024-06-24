045e779d7e6e06349a1dc639f4a7a05e12de4b31
Shalini Mane <shalinidmane@gmail.com>
2024-06-24 12:21:38 +0530

    fourth commit

--- src/Java/Small/Program/StringPattern.java
+++ src/Java/Small/Program/StringPattern.java
@@ -4,6 +4,7 @@ public class StringPattern {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("String pattern is:");
 		String s = "";
 		int n=5;
 		for (int i = 1; i <= n; ++i) {
