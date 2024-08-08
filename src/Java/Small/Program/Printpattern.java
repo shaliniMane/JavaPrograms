903925b29886d770dc28e5a063235c612a979283
Shalini Mane <shalinidmane@gmail.com>
2024-08-08 14:16:54 +0530

    pattern by shalini

--- src/Java/Small/Program/Printpattern.java
+++ src/Java/Small/Program/Printpattern.java
@@ -11,7 +11,7 @@ public class Printpattern {
 		Scanner sc = new Scanner(System.in);
 		int r=sc.nextInt();
 		System.out.println("Enter how many column:");
-	int c =	sc.nextInt();
+	 int c =	sc.nextInt();
 		System.out.println("Enter which symbol::");
 		String symbol=sc.next();
 		
