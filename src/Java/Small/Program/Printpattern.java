2d6c8a72e0793e4d385463a70c33c399c87e530b
Shalini Mane <shalinidmane@gmail.com>
2024-08-08 13:47:32 +0530

    Pattern prog by shalini

---
+++ src/Java/Small/Program/Printpattern.java
@@ -0,0 +1,29 @@
+package Java.Small.Program;
+
+import java.util.Scanner;
+
+public class Printpattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Enter how many rows:");
+		Scanner sc = new Scanner(System.in);
+		int r=sc.nextInt();
+		System.out.println("Enter how many column:");
+	int c =	sc.nextInt();
+		System.out.println("Enter which symbol:");
+		String symbol=sc.next();
+		
+		
+		for(int i=0;i<r;i++)
+		{
+			for (int j=i; j<c;j++)
+			{
+				System.out.print(symbol);
+			}
+			System.out.println();
+		}
+	}
+
+}
